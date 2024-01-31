8ef229a5ec7a22ef045aa23308e48a3f026da091
unknown <rsalazar@technisupport.com>
2024-01-31 17:59:56 -0500

    prueba mail

--- controllers/testController.js
+++ controllers/testController.js
@@ -7,3 +7,34 @@ function test(e={}){
     }
   });
 }
+
+function inlineImage() {
+  var googleLogoUrl = "https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png";
+  var youtubeLogoUrl =
+        "https://developers.google.com/youtube/images/YouTube_logo_standard_white.png";
+  var googleLogoBlob = UrlFetchApp
+                         .fetch(googleLogoUrl)
+                         .getBlob()
+                         .setName("googleLogoBlob");
+  var youtubeLogoBlob = UrlFetchApp
+                          .fetch(youtubeLogoUrl)
+                          .getBlob()
+                          .setName("youtubeLogoBlob");
+  MailApp.sendEmail({
+    to: "******@gmail.com",
+    subject: "Test App Script",
+    htmlBody: "inline Google Logo<img src='cid:googleLogo'> images! <br>" +
+              "inline YouTube Logo <img src='cid:youtubeLogo'>",
+    inlineImages:
+      {
+        googleLogo: googleLogoBlob,
+        youtubeLogo: youtubeLogoBlob
+      }
+  });
+}
+
+function cuotaMail(){
+  const cuota = MailApp.getRemainingDailyQuota();
+  Logger.log(`CuotaMail: ${cuota}`);
+  return [`CuotaMail: ${cuota}`];
+}
