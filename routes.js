6bb17e020f93d31786be5a7f375dea5adf747411
raulsalzart <raulsalzart@gmail.com>
2024-12-26 05:17:14 +0000

    deply

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const getRoutes ={
-  "test":{callback:(e)=>(test()),middelware:['isLoggedIn']}
+  // "test":{callback:(e)=>(test()),middelware:['isLoggedIn']},
+  "test":{callback:(e)=>(test())}
 }
 const postRoutes ={
   "test":{callback:(e)=>(test())}
